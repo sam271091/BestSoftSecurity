317a8c54693a7c2758b28380b79f3f8f80460968
Administrator <sam271091@gmail.com>
2021-02-05 14:41:30 +0400

    Commit

---
+++ src/sample/SecurityInstaller.java
@@ -0,0 +1,72 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+
+public class SecurityInstaller {
+
+    @FXML
+    private Tab TabSetUp2;
+
+    @FXML
+    private RadioButton RadioButtonConf1;
+
+    @FXML
+    private ToggleGroup ConfList;
+
+    @FXML
+    private RadioButton RadioButtonConf2;
+
+    @FXML
+    private RadioButton RadioButtonConf4;
+
+    @FXML
+    private RadioButton RadioButtonConf5;
+
+    @FXML
+    private TextArea TextAreaConfDescription;
+
+    @FXML
+    private RadioButton RadioFile;
+
+    @FXML
+    private ToggleGroup LocationType;
+
+    @FXML
+    private RadioButton RadioServer;
+
+    @FXML
+    private Tab TabFile;
+
+    @FXML
+    private TextField TextFieldPath;
+
+    @FXML
+    private Button ButtonOpen;
+
+    @FXML
+    private Tab TabServer;
+
+    @FXML
+    private TextField TextFieldSrvCluster;
+
+    @FXML
+    private TextField TextFileInfobase;
+
+    @FXML
+    private Tab TabResult;
+
+    @FXML
+    private Label LabelResult;
+
+    @FXML
+    private Button ButtonBack;
+
+    @FXML
+    private Button ButtonNext;
+
+}
