9469a8cceee8460d07d8e49a5725b0cc5a3136c7
Administrator <sam271091@gmail.com>
2022-06-02 15:06:33 +0400

    Commit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,33 +1,11 @@
 package sample;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-//        Parent root = FXMLLoader.load(getClass().getResource("SecLayout.fxml"));
-
-
-
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("SecLayout.fxml"));
-        Parent root = (Parent)loader.load();
-
-        SecurityInstaller controller = (SecurityInstaller)loader.getController();
-        controller.setStage(primaryStage);
-
-        primaryStage.setTitle("Best Soft: Configuration keys instalation wizard");
-        primaryStage.setResizable(false);
-        primaryStage.setScene(new Scene(root, 640, 420));
-        primaryStage.show();
-    }
-
-
+public class Main {
     public static void main(String[] args) {
-        launch(args);
+        if (args.length == 1 && args[0].equals("nogui")) {
+            System.out.println("NOGUI SELECTED");
+        } else {
+            NewMain.launch(NewMain.class, args);
+        }
     }
 }
